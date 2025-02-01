4bf4aa27d747314cf25cf853ad83edd27aeb42a9
jenkx60 <jenkinsuwagbai18@gmail.com>
2025-02-01 22:17:31 +0100

    `feature: Update Navbar component to include mobile menu functionality and update layout metadata`

--- webxela-personal-portfolio/src/app/components/Navbar.jsx
+++ webxela-personal-portfolio/src/app/components/Navbar.jsx
@@ -1,17 +1,34 @@
+"use client"
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
+import { FaBars } from 'react-icons/fa6';
+import { FaTimes } from 'react-icons/fa';
 
 const Navbar = () => {
+  const [ isMobileMenuOpen, setIsMobileMenuOpen ] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  }
   return (
     <div>
-        <nav className='fixed top-0 left-0 w-full bg-white shadow-md z-50 p-4 flex justify-between items-center text-black dark:bg-black dark:text-white'>
-            <h1 className='text-2xl font-bold'>Jenkins <span className='text-blue-300'>Uwagbai</span></h1>
-            <ul className='flex space-x-6'>
-                <li><Link href="#">Home</Link></li>
-                <li><Link href="#about">About</Link></li>
-                <li><Link href="#project">Projects</Link></li>
-                <li><Link href="#contact">Contact</Link></li>
-            </ul>
+        <nav className='fixed top-0 left-0 w-full bg-white shadow-md p-4 items-center text-black dark:bg-black dark:text-white'>
+          <div className='flex justify-between items-center w-full'>
+              <h1 className='text-2xl font-bold md:flex md:gap-2'>Jenkins <span className='text-blue-300'>Uwagbai</span></h1>
+              <button
+                onClick={toggleMobileMenu}
+                className='md:hidden focus:outline-none text-2xl' 
+              >
+                {isMobileMenuOpen ? <FaTimes /> : <FaBars />}
+              </button>
+
+              <ul className={`flex-col md:flex md:flex-row md:items-center md:space-x-6 md:relative absolute top-full left-0 w-full md:w-auto bg-black md:bg-transparent shadow-md p-4  ${isMobileMenuOpen ? 'flex' : 'hidden'}`}>
+                  <li><Link href="#" className='block py-2 md:py-0 hover:text-blue-500 transition'>Home</Link></li>
+                  <li><Link href="#about" className='block py-2 md:py-0 hover:text-blue-500 transition'>About</Link></li>
+                  <li><Link href="#project" className='block py-2 md:py-0 hover:text-blue-500 transition'>Projects</Link></li>
+                  <li><Link href="#contact" className='block py-2 md:py-0 hover:text-blue-500 transition'>Contact</Link></li>
+              </ul>
+          </div>
         </nav>
     </div>
   );
